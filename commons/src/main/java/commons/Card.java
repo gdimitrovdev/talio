7c86e0afdc0cebec4e8ffd6b47d6bbebf4791732
pedrogmo <P.GomesMoreira@student.tudelft.nl>
2023-03-06 18:43:30 +0100

    Entities and relationships working

--- commons/src/main/java/commons/Card.java
+++ commons/src/main/java/commons/Card.java
@@ -6,32 +6,41 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import javax.persistence.*;
 
-import java.util.Set;
+import java.util.List;
 
 import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
 
 @Entity
 public class Card {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     public long id;
     public String title, description, color;
 
-    @ManyToOne(optional = false, cascade = CascadeType.ALL)
+    @ManyToOne(optional = false, cascade = CascadeType.MERGE)
     @JoinColumn(name = "LIST_ID", nullable = false)
     public CardList list;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "CARD_TAG")
-    public Set<Tag> tags;
+    public List<Tag> tags;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "card")
-    public Set<Subtask> subtasks;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "card", fetch = FetchType.EAGER)
+    public List<Subtask> subtasks;
 
     public Card(){}
 
-    public Card(long id, String title, String description, String color, CardList list, Set<Tag> tags, Set<Subtask> subtasks) {
+    public Card(long id, String title, String description, String color, CardList list, List<Tag> tags, List<Subtask> subtasks) {
         this.id = id;
+        this.title = title;
+        this.description = description;
+        this.color = color;
+        this.list = list;
+        this.tags = tags;
+        this.subtasks = subtasks;
+    }
+
+    public Card(String title, String description, String color, CardList list, List<Tag> tags, List<Subtask> subtasks) {
         this.title = title;
         this.description = description;
         this.color = color;
--- commons/src/main/java/commons/Tag.java
+++ commons/src/main/java/commons/Tag.java
@@ -6,28 +6,35 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import javax.persistence.*;
 
-import java.util.Set;
+import java.util.List;
 
 import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
 
 @Entity
 public class Tag {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     public long id;
     public String title, color;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.MERGE)
     @JoinColumn(name = "BOARD_ID", nullable = false)
     public Board board;
 
-    @ManyToMany(mappedBy = "tags")
-    public Set<Card> cards;
+    @ManyToMany(fetch = FetchType.EAGER, mappedBy = "tags")
+    public List<Card> cards;
 
     public Tag(){}
 
-    public Tag(long id, String title, String color, Board board, Set<Card> cards) {
+    public Tag(long id, String title, String color, Board board, List<Card> cards) {
         this.id = id;
+        this.title = title;
+        this.color = color;
+        this.board = board;
+        this.cards = cards;
+    }
+
+    public Tag(String title, String color, Board board, List<Card> cards) {
         this.title = title;
         this.color = color;
         this.board = board;
