525e726b967d05626712e6ea0efd2393da7467f6
Georgi Dimitrov <g.h.dimitrov@student.tudelft.nl>
2023-03-06 17:01:46 +0100

    Created some controllers and services on the server

---
+++ server/src/main/java/server/services/CardService.java
@@ -0,0 +1,41 @@
+package server.services;
+
+import commons.Card;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import server.database.CardRepository;
+
+import java.util.List;
+
+@Service
+public class CardService {
+
+    private CardRepository cardRepository;
+
+    public CardService(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+
+    public List<Card> getMany() {
+        return cardRepository.findAll();
+    }
+
+    public ResponseEntity<Card> getOne(Long id) {
+        if (id < 0 || !cardRepository.existsById(id)) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(cardRepository.getById(id));
+    }
+
+    public ResponseEntity<Card> createOne(Card card) {
+        Card newCard = cardRepository.save(card);
+        return ResponseEntity.ok(newCard);
+    }
+
+    public void deleteOne(Long id) {
+        cardRepository.deleteById(id);
+    }
+
+}
