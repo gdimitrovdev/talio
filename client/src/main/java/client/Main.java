81b6d2fd2a9171c4ecd21166ad6e6494d814d0a8
Dimana Stoyanova <d.stoyanova@student.tudelft.nl>
2023-03-10 17:57:24 +0100

    created UI board and list elements. Ref #25 #26

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -20,6 +20,7 @@ import static com.google.inject.Guice.createInjector;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import client.scenes.BoardCtrl;
 import com.google.inject.Injector;
 
 import client.scenes.AddQuoteCtrl;
@@ -44,6 +45,6 @@ public class Main extends Application {
         var add = FXML.load(AddQuoteCtrl.class, "client", "scenes", "AddQuote.fxml");
 
         var mainCtrl = INJECTOR.getInstance(MainCtrl.class);
-        mainCtrl.initialize(primaryStage, overview, add);
+        mainCtrl.initialize(primaryStage, overview, add );
     }
 }
---
+++ client/src/main/java/elements/BoardElement.java
@@ -0,0 +1,77 @@
+package elements;
+
+import client.scenes.MainCtrlTalio;
+import commons.Board;
+import commons.CardList;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.*;
+
+
+public class BoardElement  extends ScrollPane {
+    private final MainCtrlTalio mainCtrlTalio;
+
+    private HBox innerHBox;
+
+
+    public BoardElement(Board board, MainCtrlTalio mainCtrlTalio)  {
+        super();
+        this.mainCtrlTalio = mainCtrlTalio;
+
+        this.setLayoutX(10.0);
+        this.setLayoutY(40.0);
+
+
+        //create the UI components
+        HBox outerHBox = new HBox();
+        HBox innerHBox = new HBox();
+        Button newListButton = new Button("+");
+        this.innerHBox=innerHBox;
+
+        // set the style for the UI components
+        outerHBox.setStyle(" -fx-min-width: 780.0; -fx-min-height: 510.0; -fx-background-color: #d3dbe8;");
+        outerHBox.setPadding(new Insets(40, 0, 0, 40));
+        outerHBox.setSpacing(80);
+        newListButton.setStyle("-fx-pref-width: 30px; -fx-pref-height: 30px;");
+        Tooltip newListToolTip = new Tooltip("add a new list");
+        newListButton.setTooltip(newListToolTip);
+
+        //add the CardListElement instances to the innerHBox
+        for(CardList cardList : board.lists){
+            CardListElement cardListEl = new CardListElement(cardList, mainCtrlTalio);
+            innerHBox.getChildren().add(cardListEl);
+            innerHBox.setMargin(cardListEl, new Insets(0, 10, 0, 0));
+        }
+        //add the innerHBox and newListButton to the outerHBox
+        outerHBox.getChildren().add(innerHBox);
+        outerHBox.getChildren().add(newListButton);
+
+        // Wrap the outerHBox in a StackPane
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(outerHBox);
+
+        // Set the content of the ScrollPane to the StackPane
+        setContent(stackPane);
+
+        // Set the vertical scrollbar to always be shown
+        //and the horizontal scrollbar to never be shown
+        setHbarPolicy(ScrollBarPolicy.ALWAYS);
+        setVbarPolicy(ScrollBarPolicy.NEVER);
+
+        VBox.setVgrow(this, Priority.ALWAYS);
+
+    }
+    //TODO implement this method
+    public void addCardList(){
+
+        CardList current = new CardList();
+        current.title="Enter list title";
+        innerHBox.getChildren().add(new CardListElement(current, mainCtrlTalio));
+        //TO DO: the id of the list and the id of the board
+        //attributes of the cardList should be set here
+
+    }
+
+}
