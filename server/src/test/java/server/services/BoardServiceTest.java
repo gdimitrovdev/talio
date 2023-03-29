59cd705665be3914f86b708c64cd20a515f86c8f
Dimana Stoyanova <d.stoyanova@student.tudelft.nl>
2023-03-29 21:34:10 +0200

    wrote tests for the CardListService.java

--- server/src/test/java/server/services/BoardServiceTest.java
+++ server/src/test/java/server/services/BoardServiceTest.java
@@ -26,7 +26,7 @@ class BoardServiceTest  {
 
     //@InjectMocks annotation is used to inject the mock BoardRepository object into the BoardService object being tested.
     @InjectMocks
-    private BoardService boardService;
+    private BoardService boardServiceMock;
 
 
     @BeforeEach
@@ -34,112 +34,103 @@ class BoardServiceTest  {
         MockitoAnnotations.openMocks(this);
     }
     @Test
-    public void testGetMany() {
+    public void getMany() {
         List<Board> expectedBoards = new ArrayList<>();
         expectedBoards.add(new Board("board1", "code1", "readOnlyCode1", "red"));
         expectedBoards.add(new Board("board2", "code2", "readOnlyCode2", "blue"));
         when(boardRepositoryMock.findAll()).thenReturn(expectedBoards);
 
-        List<Board> actualBoards = boardService.getMany();
+        List<Board> actualBoards = boardServiceMock.getMany();
 
         assertEquals(expectedBoards, actualBoards);
     }
 
 
     @Test
-    public void testGetOne() {
+    public void getOne() {
         Long boardId = 1L;
         Board expectedBoard = new Board("board1", "code1", "readOnlyCode1", "red");
         when(boardRepositoryMock.findById(boardId)).thenReturn(Optional.of(expectedBoard));
 
-        Optional<Board> actualBoard = boardService.getOne(boardId);
+        Optional<Board> actualBoard = boardServiceMock.getOne(boardId);
         assertEquals(expectedBoard, actualBoard.get());
     }
 
 
     @Test
-    public void testGetOneWithInvalidId() {
+    public void getOneWithInvalidId() {
         Optional<Board> optionalBoard = Optional.empty();
         when(boardRepositoryMock.findById(1L)).thenReturn(optionalBoard);
 
-        Optional<Board> result = boardService.getOne(1L);
+        Optional<Board> result = boardServiceMock.getOne(1L);
 
         assertThat(result).isEmpty();
     }
     @Test
-    public void testGetOneByCode() {
+    public void getOneByCode() {
         Board board = new Board();
         board.setCode("12345");
         Optional<Board> optionalBoard = Optional.of(board);
         when(boardRepositoryMock.findByCode("12345")).thenReturn(optionalBoard);
-        Optional<Board> result = boardService.getOneByCode("12345");
+        Optional<Board> result = boardServiceMock.getOneByCode("12345");
         assertEquals("12345", result.get().getCode());
     }
 
 
     @Test
-    public void testGetOneByInvalidCode() {
+    public void getOneByInvalidCode() {
         Optional<Board> optionalBoard = Optional.empty();
         when(boardRepositoryMock.findByCode("12345")).thenReturn(optionalBoard);
-        Optional<Board> result = boardService.getOneByCode("12345");
+        Optional<Board> result = boardServiceMock.getOneByCode("12345");
         assertThat(result).isEmpty();
     }
     @Test
-    public void testCreateOne() {
+    public void createOne() {
         Board board = new Board();
-        board.setName("Test Board");
-        board.setColor("#FF0000");
 
-        Board savedBoard = new Board();
-        savedBoard.setId(1L);
-        savedBoard.setName(board.getName());
-        savedBoard.setColor(board.getColor());
+        when(boardRepositoryMock.save(ArgumentMatchers.any())).thenReturn(board);
 
-        when(boardRepositoryMock.save(ArgumentMatchers.any())).thenReturn(savedBoard);
-
-        Board createdBoard = boardService.createOne(board);
-        assertEquals(savedBoard, createdBoard);
+        Board createdBoard = boardServiceMock.createOne(board);
+        assertEquals(board, createdBoard);
     }
 
-
+    // Test whether deleteById() method of the repository was called for the correct board
     @Test
-    public void testDeleteOne() {
-        Long boardId = 1L;
+    public void deleteOne() {
 
-        when(boardRepositoryMock.existsById(boardId)).thenReturn(true);
+        when(boardRepositoryMock.existsById(1L)).thenReturn(true);
 
-        boardService.deleteOne(boardId);
+        boardServiceMock.deleteOne( 1L);
 
-        verify(boardRepositoryMock).deleteById(boardId);
+        verify(boardRepositoryMock).deleteById(1L);
 
     }
 
 
     @Test
-    public void testUpdateOne() {
-        Long boardId = 1L;
+    public void updateOne() {
 
         Board existingBoard = new Board();
-        existingBoard.setId(boardId);
+        existingBoard.setId(1L);
         existingBoard.setName("Test Board");
         existingBoard.setColor("#red");
         existingBoard.setCode("abc");
         existingBoard.setReadOnlyCode("a");
 
         Board updatedBoard = new Board();
-        updatedBoard.setId(boardId);
+        updatedBoard.setId(2L);
         updatedBoard.setName("Updated Board");
         updatedBoard.setColor("#blue");
         updatedBoard.setCode("abc");
         updatedBoard.setReadOnlyCode("b");
 
 
-        when(boardRepositoryMock.findById(boardId)).thenReturn(Optional.of(existingBoard));
+        when(boardRepositoryMock.findById(1L)).thenReturn(Optional.of(existingBoard));
         when(boardRepositoryMock.findByCode(updatedBoard.getCode())).thenReturn(Optional.empty());
         when(boardRepositoryMock.findByCode(updatedBoard.getReadOnlyCode())).thenReturn(Optional.empty());
         when(boardRepositoryMock.save(existingBoard)).thenReturn(existingBoard);
 
-        Board result = boardService.updateOne(boardId, updatedBoard);
+        Board result = boardServiceMock.updateOne(1L, updatedBoard);
 
         assertEquals(result, existingBoard);
         assertEquals(existingBoard.getName(), updatedBoard.getName());
@@ -149,27 +140,33 @@ class BoardServiceTest  {
 
     }
     @Test
-    public void testIsCodeAlreadyUsed() {
+    public void isCodeAlreadyUsed() {
         String code = "123";
         Board board = new Board();
         board.setCode(code);
 
         when(boardRepositoryMock.findByCode(code)).thenReturn(Optional.of(board));
 
-        Assertions.assertEquals(boardService.isCodeAlreadyUsed(code), true);
+        Assertions.assertEquals(boardServiceMock.isCodeAlreadyUsed(code), true);
     }
     @Test
-    public void testUpdateBoardCodes() {
+    public void updateBoardCodes() {
         Board board = new Board();
         String firstCode = board.getCode();
         String readOnlyCode = board.getReadOnlyCode();
 
-        boardService.updateBoardCodes(board);
+        boardServiceMock.updateBoardCodes(board);
 
         Assertions.assertNotEquals(firstCode, board.getCode());
         Assertions.assertNotEquals(readOnlyCode, board.getReadOnlyCode());
 
+    }
 
+    @Test
+    public void getRandomCode() {
+        String code1 = boardServiceMock.getNewRandomCode(4);
+        String code2 = boardServiceMock.getNewRandomCode(4);
+        Assertions.assertNotEquals(code1, code2);
 
     }
 
---
+++ server/src/test/java/server/services/CardListServiceTest.java
@@ -0,0 +1,103 @@
+package server.services;
+
+import commons.CardList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import server.database.BoardRepository;
+import server.database.CardListRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+class CardListServiceTest {
+
+    // @Mock annotation is used to create a mock BoardRepository object
+    @Mock
+    private CardListRepository cardListRepositoryMock;
+
+    @InjectMocks
+    private CardListService cardListServiceMock;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void getMany() {
+        // Create a list of CardList objects for testing purposes
+        List<CardList> cardLists = new ArrayList<>();
+        CardList cardList1 = new CardList();
+        CardList cardList2 = new CardList();
+        cardLists.add(cardList1);
+        cardLists.add(cardList2);
+
+        // Specifying the behavior of the findAll() method of the CardListRepository mock
+        when(cardListRepositoryMock.findAll()).thenReturn(cardLists);
+
+        List<CardList> returnedCardLists = cardListServiceMock.getMany();
+        assertEquals(cardList1, returnedCardLists.get(0));
+        assertEquals(cardList2, returnedCardLists.get(1));
+    }
+
+
+    @Test
+    void getOne() {
+        Long listId = 1L;
+        CardList cardList = new CardList();
+        when(cardListRepositoryMock.findById(listId)).thenReturn(Optional.of(cardList));
+
+        Optional<CardList> returnedCardList = cardListServiceMock.getOne(1L);
+        assertEquals(cardList, returnedCardList.get());
+
+
+    }
+
+
+    @Test
+    void createOne() {
+        CardList cardList = new CardList();
+
+        when(cardListRepositoryMock.save(cardList)).thenReturn(cardList);
+
+        CardList returnedCardList = cardListServiceMock.createOne(cardList);
+        assertEquals(cardList, returnedCardList);
+    }
+
+    // Test whether deleteById() method of the repository was called for the correct cardList
+    @Test
+    void deleteOne() {
+        when(cardListRepositoryMock.existsById(1L)).thenReturn(true);
+
+        cardListServiceMock.deleteOne(1L);
+
+        verify(cardListRepositoryMock).deleteById(1L);
+    }
+
+    @Test
+    void updateOne() {
+        CardList cardList = new CardList();
+        cardList.setId(1L); //setting the id and title manually because otherwise all constructors require a Board
+        cardList.setTitle("title1");
+
+        when(cardListRepositoryMock.findById(1L)).thenReturn(Optional.of(cardList));
+        when(cardListRepositoryMock.save(cardList)).thenReturn(cardList);
+
+        CardList updatedCardList = new CardList();
+        updatedCardList.setTitle("title2");
+        CardList returnedCardList = cardListServiceMock.updateOne(1L, updatedCardList);
+        assertEquals(updatedCardList.getTitle(), returnedCardList.getTitle());
+
+    }
+}
